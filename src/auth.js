c0182675309f677456923e96fbeb763422a19f6d
Rafael <rafaelbertelli89@gmail.com>
2019-07-14 10:03:56 -0300

    Create private router

---
+++ src/auth.js
@@ -0,0 +1,7 @@
+/**
+ * @isAuthenticated
+ * Methods to verify auth must be defined into it.
+ * Here, we are mocking 'true' or 'false' simulating back-end response
+ * */
+export const isAuthenticated = () => true;
+// export const isAuthenticated = () => false;
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,31 @@
 import React from "react";
-import { BrowserRouter, Route, Link, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { isAuthenticated } from "./auth";
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      isAuthenticated() ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{ pathname: "/login", state: { from: props.location } }}
+        />
+      )
+    }
+  />
+);
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={() => <h1>Oiêêê</h1>} />
       <Route path="/login" component={() => <h1>Login</h1>} />
+      <PrivateRoute
+        exact
+        path="/"
+        component={() => <h1>Você está logado</h1>}
+      />
     </Switch>
   </BrowserRouter>
 );
